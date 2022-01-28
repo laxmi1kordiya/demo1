b8c97604b57c1a27ea1eafe94f1e408d2c010a7b
laxmi1kordiya <97527915+laxmi1kordiya@users.noreply.github.com>
2022-01-28 10:19:51 +0530

    pseudo class  learn to css property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,16 @@ import { PortfolioComponent } from './app/portfolio/portfolio.component';
 import { AllComponent } from './app/all/all.component';
 import { RotateComponent } from './app/css property/rotate.component';
 import { JobsFairComponent } from './app/jobs-fair/jobs-fair.component';
+import { BorderComponent } from './cssproperty/border/border.component';
+import { PseudoComponent } from './pseudo/pseudo.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes:Routes=[
+  {path:'Home',component: AllComponent},
+  {path:'about-us',component: JobsFairComponent },
+  {path:'service',component: ServiceComponent}
+
+];
 
 @NgModule({
   declarations: [
@@ -17,13 +27,16 @@ import { JobsFairComponent } from './app/jobs-fair/jobs-fair.component';
     PortfolioComponent,
     AllComponent,
     RotateComponent,
-    JobsFairComponent
+    JobsFairComponent,
+    BorderComponent,
+    PseudoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CarouselModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pseudo/pseudo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pseudo',
+  templateUrl: './pseudo.component.html',
+  styleUrls: ['./pseudo.component.scss']
+})
+export class PseudoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
